cef4af24a2bf9c4d8366070c18d486fb7c10088c
Ryan Wong <rewong03@gmail.com>
2022-07-13 14:35:42 -0400

    Added typing to utils

--- ryan/control_flow/cpp_utils.py
+++ ryan/control_flow/cpp_utils.py
@@ -1,44 +1,49 @@
-from cpp_parser import *
-from typing import *
+from cpp_parser import Token, Variable
+from typing import List
 
-def get_statement_tokens(t):
-    """ #type: Token -> List[Token]
+def get_statement_tokens(token: Token) -> List[Token]:
+    """
     Returns all tokens involved in a statement in order
     """
-    if not t:
+    if not token:
         return []
-    elif not(t.astOperand1 or t.astOperand2):
-        return [t]
+    elif not(token.astOperand1 or token.astOperand2):
+        return [token]
     
-    return get_statement_tokens(t.astOperand1) + [t] + get_statement_tokens(t.astOperand2)
+    return get_statement_tokens(token.astOperand1) + [token] + get_statement_tokens(token.astOperand2)
 
-def get_vars_from_statement(t):
-    """ #type: List[Token] -> List[Token]
+def get_vars_from_statement(tokens: List[Token]) -> List[Variable]:
+    """
     Returns a list of variable tokens from a list of tokens
     """
-    return list(filter(lambda x: x.varId, t))
+    variables = []
+    for t in tokens:
+        if t.variable:
+            variables.append(t.variable)
 
-def get_LHS_from_statement(t):
-    """ #type: List[Token] -> List[Token]
+    return variables
+
+def get_LHS_from_statement(tokens: List[Token]) -> List[Token]:
+    """
     Returns the tokens of the LHS of an expression
     """
-    for i in range(len(t)):
-        if "=" in t[i].str:
-            return t[:i]
+    for i in range(len(tokens)):
+        if "=" == tokens[i].str:
+            return tokens[:i]
 
-def get_RHS_from_statement(t):
-    """ #type: List[Token] -> List[Token]
+def get_RHS_from_statement(tokens: List[Token]) -> List[Token]:
+    """
     Returns the tokens of the RHS of an expression
     """
-    for i in range(len(t)):
-        if "=" in t[i].str:
-            return t[i:]
+    for i in range(len(tokens)):
+        if "=" == tokens[i].str:
+            return tokens[i:]
 
-def tokens_to_str(t):
-    """ #type: List[Token] -> List[str]
+def tokens_to_str(tokens: List[Token]) -> List[str]:
+    """
     Returns a list of strings extracted from a list of tokens
     """
-    return list(map(lambda x: x.str, t))
+    return list(map(lambda x: x.str, tokens))
 
 def root_token_to_str(t: Token) -> str:
     if not t:
