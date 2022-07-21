b947a084e7b518164473cb692984287143f244c2
Ryan Wong <rewong03@gmail.com>
2022-07-20 21:52:10 -0400

    Dependency graph generation; untested

--- ryan/control_flow/dependency_graph.py
+++ ryan/control_flow/dependency_graph.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 from collections import deque
-from typing import Dict, Set, Tuple
+from typing import Dict, List, Optional, Set
 
 import attr
 
@@ -56,56 +58,113 @@ def create_def_use_pairs(cfg: FunctionCFG) -> Dict[CFGNode, DefUsePair]:
     return def_use_pairs
 
 
-def reach_definitions(cfg: FunctionCFG):
-    """Calculates variables that reach a node for all nodes in CFG"""
-    reach_out: Dict[CFGNode, Set[Tuple[CFGNode, Variable]]] = {}
-    reach: Dict[CFGNode, Set[Tuple[CFGNode, Variable]]] = {}
+@attr.s()
+class ReachDef:
+    """Data class of a variable dn the CFGNode which defines it"""
+    def_node: CFGNode = attr.ib()
+    variable: Variable = attr.ib()
+
+
+def reach_definitions(cfg: FunctionCFG) -> Dict[CFGNode, Set[ReachDef]]:
+    """Calculates variables that reach a node for all nodes in CFG. Returns a mapping 
+    between CFGNodes and ReachNodes.
+    """
+    reach_out: Dict[CFGNode, Set[ReachDef]] = {}
+    reach: Dict[CFGNode, Set[ReachDef]] = {}
     for n in cfg.nodes:
         reach_out[n] = set()
         reach[n] = set()
 
-    def_use_pairs: Dict[CFGNode, Dict[str, Set[Variable]]] = create_def_use_pairs(cfg)
+    def_use_pairs: Dict[CFGNode, DefUsePair] = create_def_use_pairs(cfg)
 
     queue = deque(cfg.nodes)
 
     while queue:
         cur: CFGNode = queue.pop()
-        old_reach_out: Set[Tuple(CFGNode, Variable)] = reach_out[cur]
+        old_reach_out: Set[ReachDef] = reach_out[cur]
 
         reach[cur] = set()
         for prev in cur.previous:
             reach[cur].update(reach_out[prev])
 
-        gen: Set[Tuple[CFGNode, Variable]] = set()
+        gen: Set[ReachDef] = set()
         kill: Set[Variable] = set()
-        new_reach_out: Dict[CFGNode, Set[Tuple[CFGNode, Variable]]] = set()
+        new_reach_out: Dict[CFGNode, Set[ReachDef]] = set()
         if def_use_pairs[cur].define:
             for def_var in def_use_pairs[cur].define:
-                gen.add((cur, def_var))
+                gen.add(ReachDef(cur, def_var))
                 kill.add(def_var)
 
             new_reach_out.update(gen)
             for node, variable in reach[cur]:
                 if variable not in kill:
-                    new_reach_out.add((node, variable))
+                    new_reach_out.add(ReachDef(node, variable))
+        else:  # If nothing is defined then then kill and gen are empty sets
+            new_reach_out = reach[cur]
 
         reach_out[cur] = new_reach_out
 
         if new_reach_out != old_reach_out:
             queue.extend(cur.next)
 
-    for cur_node, reach_tuples in reach.items():
-        cur_def = set(def_use_pairs[cur_node].define)
-        cur_use = set(def_use_pairs[cur_node].use)
+    return reach
+
+
+@attr.s()
+class DependencyNode:
+    cfgnode: CFGNode = attr.ib()
+    variable: Variable = attr.ib()
+    next: Set[DependencyNode] = attr.ib(factory=set)
+    previous: Set[DependencyNode] = attr.ib(factory=set)
+
+
+def create_dependency_graph(reach_definitions: Dict[CFGNode, Set[ReachDef]], 
+                            def_use_pairs: Dict[CFGNode, DefUsePair]) -> List[DependencyNode]:
+    # Maps CFGNode to set of ReachDef which represent variables which are used to define other variables
+    # in CFGNode
+    node_dependency_mapping: Dict[CFGNode, Set[ReachDef]] = {}
+    
+    # Get all variables which are used to define other variables in each node
+    for cur_node, reach_def in reach_definitions.items():
+        cur_def = def_use_pairs[cur_node].define
+        cur_use = def_use_pairs[cur_node].use
         remove = set()
 
-        for reach_node, reach_var in reach_tuples:
+        if not cur_def:
+            continue
+
+        for r in reach_def:
+            reach_var = r.variable
+            # If the variable is killed by the statement or if variable isn't used in statement
             if reach_var in cur_def or reach_var not in cur_use:
-                remove.add((reach_node, reach_var))
+                remove.add(reach_def)
 
-        reach[cur_node] -= remove
+        reach_definitions[cur_node] -= remove
 
-    return reach
+        node_dependency_mapping[cur_node] = reach_definitions[cur_node]
+
+    cfg_dependency_node_mapping = {}  # Maps CFGNode to set of DependencyNodes
+    dependency_graph_nodes = []  # All DependencyNodes
+    # Create all nodes of dependency graph for all variables in each node
+    for cur_node, reach_def in node_dependency_mapping.items():
+        cfg_dependency_node_mapping[cur_node] = set()
+        cur_def = def_use_pairs[cur_node].define
+
+        for def_var in cur_def:
+            d_node = DependencyNode(cur_node, def_var)
+            dependency_graph_nodes.append(d_node)
+            cfg_dependency_node_mapping[cur_node].add(d_node)
+
+    # Set previous and next nodes for all dependency nodes
+    for d in dependency_graph_nodes:
+        reach_def = node_dependency_mapping[d.cfgnode]
+
+        for r in reach_def:
+            prev = cfg_dependency_node_mapping[r.def_node]
+            d.previous.add(prev)
+            prev.next = d
+
+    return dependency_graph_nodes
 
 
 if __name__ == "__main__":
